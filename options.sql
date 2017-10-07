32e3921e3030091f04905844a08dbb888f2b72f2
折影轻梦 <776194970@qq.com>
2017-10-07 10:10:32 +0800

    修复phpmyadmin不显示修改的问题

--- options.sql
+++ options.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 2017-09-10 10:14:40
+-- Generation Time: 2017-10-07 10:09:15
 -- 服务器版本： 5.6.32-log
 -- PHP Version: 7.1.2
 
@@ -27,19 +27,39 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `options` (
-  `name` text NOT NULL,
-  `value` text NOT NULL
+  `id` int(255) NOT NULL,
+  `name` text,
+  `value` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 转存表中的数据 `options`
 --
 
-INSERT INTO `options` (`name`, `value`) VALUES
-('title', '一中树洞'),
-('keywords', '表白,树洞,微信表白墙,校园表白墙,表白墙'),
-('description', '趁青春！');
+INSERT INTO `options` (`id`, `name`, `value`) VALUES
+(1, 'title', '觅思树洞'),
+(2, 'keywords', '表白,树洞,微信表白墙,校园表白墙,表白墙'),
+(3, 'description', '我说的话，不知远方的人听见了吗？');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
